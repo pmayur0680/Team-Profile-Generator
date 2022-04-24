925e249f61c3e1a5bfc86ed714289082bebd1d1d
Mayur Patel <pmayur0680@gmail.com>
2022-04-23 22:47:29 -0500

    defined properties and methods for all other classes

---
+++ index.js
@@ -0,0 +1,12 @@
+// Include packages needed for this application
+const inquirer = require('inquirer'); // interactive command line user interfaces
+const fs = require('fs'); // Node standard library package for reading and writing files
+
+// Create a function to write html file
+function writeToFile(data) {
+}
+// Create a function to initialize app
+function init() {
+}
+// Function call to initialize app
+init();
